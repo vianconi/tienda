b24a481f5ccbab8931f361044e7edf1566d67782
Emilio Villalba <emiliovianconi@gmail.com>
2025-02-06 09:13:13 -0300

    se agregan los primeros archivos

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+def pagina_no_encontrada(error):
+    return render_template('errores/404.html'), 404
+
+def inicializar_app(config):
+    app.config.from_object(config)
+    app.register_error_handler(404, pagina_no_encontrada)
+    return app
+
